2054997980dcff5ab84f777df4068c5fff855a05
SeabassTheFish03 <sneumannthesecond@gmail.com>
2024-11-26 09:14:09 -0500

    more Turing progress

--- test_scenes.py
+++ test_scenes.py
@@ -1,27 +1,28 @@
 # Standard Lib
 import tomllib
-import json
 from manim._config import tempconfig
 
 # Manim
 from manim.scene.scene import Scene
+from manim.animation.fading import FadeIn
 
 # TheoryViz
+from fa_manager import DFA_Manager
 from text_visuals import TuringTape
 
 
-class TuringTapeTest(Scene):
+class TMTapeTest(Scene):
     def construct(self):
         with open("config_template.toml", "rb") as f:
             config = tomllib.load(f)
 
         test_tape = TuringTape("abc", "_", config)
-        self.add(test_tape.mobj)
-        self.play(test_tape.animate_change_highlighted(1))
-        self.wait()
+
+        self.play(FadeIn(test_tape))
+        self.play(test_tape.animate_update("right"))
 
 
 if __name__ == "__main__":
     with tempconfig({"quality": "low_quality", "preview": True}):
-        scene = TuringTapeTest()
+        scene = TMTapeTest()
         scene.render()
